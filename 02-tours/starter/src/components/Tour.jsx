4a56c02448aa702b1a64457c68a5ecf8b5acab36
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-03 17:41:58 +0100

    added the read more functionality dynamically using substring javascript method at the tour.jsx

--- 02-tours/starter/src/components/Tour.jsx
+++ 02-tours/starter/src/components/Tour.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useState } from "react";
 
 const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+  console.log(info.substring(0, 10));
+
   return (
     <>
       <article className="single-tour">
@@ -8,7 +12,16 @@ const Tour = ({ id, image, info, name, price, removeTour }) => {
         <span className="tour-price">${price}</span>
         <div className="tour-info">
           <h5>{name}</h5>
-          <p>{info}</p>
+          <p>
+            {readMore ? info : `${info.substring(0, 200)}...`}
+            <button
+              className="info-btn"
+              type="button"
+              onClick={() => setReadMore(!readMore)}
+            >
+              {readMore ? "show less" : "read more"}
+            </button>
+          </p>
 
           <button
             type="button"
