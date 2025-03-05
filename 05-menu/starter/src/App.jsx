5903753b126d09267611216757cb65af990b33f9
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-05 10:00:14 +0100

    started a new project to display each menu item from the data.js file to render in the menuItem.jsx file

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+import Title from "./components/Title";
+import menu from "./data";
+import Menu from "./components/Menu";
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
