4bc31e7f7d6aac68e997754d6ff2ac6f25f6dd98
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-05 10:19:19 +0100

    `rendered the categories item to the ui

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Title from "./components/Title";
 import menu from "./data";
 import Menu from "./components/Menu";
+import Categories from "./components/Categories";
 
 // const tempCategories = menu.map((item) => item.category);
 // const tempSet = new Set(tempCategories);
@@ -15,12 +16,11 @@ const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
 
-  console.log(categories);
-
   return (
     <main>
       <section className="menu">
         <Title text="our menu" />
+        <Categories categories={categories} />
         <Menu items={menuItems} />
       </section>
     </main>
