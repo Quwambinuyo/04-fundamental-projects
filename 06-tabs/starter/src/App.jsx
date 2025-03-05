8dac1086bc9e29eb339a620504e01ba752e9726b
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-05 12:29:23 +0100

    successfully finished setting up the jobs

--- 06-tabs/starter/src/App.jsx
+++ 06-tabs/starter/src/App.jsx
@@ -1,6 +1,34 @@
-const url = 'https://www.course-api.com/react-tabs-project';
+import { useEffect } from "react";
+import { useState } from "react";
+
+const url = "https://www.course-api.com/react-tabs-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  // Current Item
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section jobs-center>
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;
