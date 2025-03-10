a99f3ca313811adbfdffa284e35d2648781152f2
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-10 10:16:42 +0100

    feat: add color input functionality to dynamically update color list with error handling

--- 09-color-generator/starter/src/App.jsx
+++ 09-color-generator/starter/src/App.jsx
@@ -7,10 +7,19 @@ import { ToastContainer, toast } from "react-toastify";
 const App = () => {
   const [colors, setColors] = useState(new Values("#f15025").all(10));
 
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(10);
+      setColors(newColors);
+    } catch (error) {
+      toast.error("not a valid hex input");
+    }
+  };
+
   return (
     <>
       <main>
-        <Form />
+        <Form addColor={addColor} />
         <ColorList colors={colors} />
         <ToastContainer position="top-center" />
       </main>
