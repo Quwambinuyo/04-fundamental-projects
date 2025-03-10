82ed52e78a292e0e165f5a52ccadeb4d6df3a082
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-10 09:07:21 +0100

    setting up the structure for my color generator project

---
+++ 09-color-generator/starter/src/components/Form.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [color, setColor] = useState("");
+  const handleSubmit = (e) => {};
+  return (
+    <>
+      <section className="container">
+        <h4>Color Generator</h4>
+        <form className="color-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+
+          <button
+            type="submit"
+            value=""
+            className="btn"
+            style={{ background: color }}
+          >
+            Submit
+          </button>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Form;
