7267e49f96a7dba32085b21c077198a132f200a7
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-10 16:15:38 +0100

    feat: add checkbox functionality to SingleItem component for task completion tracking

--- 10-grocery-bud/starter/src/components/SingleItem.jsx
+++ 10-grocery-bud/starter/src/components/SingleItem.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
-const SingleItem = () => {
-  return <div>SingleItem</div>;
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(item.completed);
+
+  return (
+    <>
+      <div className="single-item">
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={() => setIsChecked(!isChecked)}
+        />
+        <p
+          style={{
+            textTransform: "capitalize",
+            textDecoration: isChecked && "line-through",
+          }}
+        >
+          {item.name}
+        </p>
+        <button type="button" className="btn remove-btn">
+          Delete
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default SingleItem;
