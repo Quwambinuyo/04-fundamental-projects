614d22dd14a835663f0c267cda5f678e5140556b
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-10 15:42:53 +0100

    feat: implement form component for adding grocery items with state management

---
+++ 10-grocery-bud/starter/src/components/Form.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+
+const Form = () => {
+  const [newItemName, setNewItemName] = useState("");
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    console.log(newItemName);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSumbit}>
+        <h4>grocery bud</h4>
+        <div className="form-control">
+          <input
+            type="text"
+            className="form-input"
+            value={newItemName}
+            onChange={(e) => setNewItemName(e.target.value)}
+          />
+          <button type="submit" className="btn">
+            Add item
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Form;
