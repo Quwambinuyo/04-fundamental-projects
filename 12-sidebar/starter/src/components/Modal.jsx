92be4110a9c8640863dc25c51593eb167dd1395b
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-17 15:46:27 +0100

    implement modal component with open/close functionality and associated styles

--- 12-sidebar/starter/src/components/Modal.jsx
+++ 12-sidebar/starter/src/components/Modal.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
+import { useGlobalCotext } from "../context/Context";
 
 const Modal = () => {
-  return <div>Modal</div>;
+  const { isModalOpen, closeModal } = useGlobalCotext();
+  return (
+    <>
+      <div
+        className={isModalOpen ? "modal-overlay show-modal" : "modal-overlay"}
+      >
+        <div className="modal-container">
+          <h3>Modal Content</h3>
+          <button className="close-modal-btn" onClick={closeModal}>
+            <FaTimes />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
