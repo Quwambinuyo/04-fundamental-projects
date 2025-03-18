6b4ef6f746e91688f3c785852e4efb205b6934bd
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-18 11:38:05 +0100

    add mouse over functionality to Navbar for resetting pageId; integrate setPageId from GlobalContext

--- 13-strapi-submenus/starter/src/components/Navbar.jsx
+++ 13-strapi-submenus/starter/src/components/Navbar.jsx
@@ -4,20 +4,23 @@ import { useGlobalContext } from "../context/GlobalContext";
 import NavLinks from "./NavLinks";
 
 const Navbar = () => {
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext();
+  const handleSubmenu = (e) => {
+    // console.log(e.target);
+    if (!e.target.classList.contains("nav-link")) {
+      setPageId(null);
+    }
+  };
   return (
-    <>
-      <nav>
-        <div className="nav-center">
-          <h3 className="logo">strapi</h3>
-          <button className="toggle-btn" onClick={openSidebar}>
-            <FaBars />
-          </button>
-          <NavLinks />
-        </div>
-      </nav>
-    </>
+    <nav onMouseOver={handleSubmenu}>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+        <NavLinks />
+      </div>
+    </nav>
   );
 };
-
 export default Navbar;
