9a2f9639c5a0de6b7537be51513d929bfd38b294
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-18 10:11:21 +0100

    update Hero component with new layout and styles for improved content management presentation

--- 13-strapi-submenus/starter/src/components/Hero.jsx
+++ 13-strapi-submenus/starter/src/components/Hero.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const Hero = () => {
   return (
     <>
-      <h2>Hero</h2>
+      <div className="hero-container">
+        <div className="hero-center">
+          <h1>
+            Manage Any Content <br /> Anywhere
+          </h1>
+          <p>
+            Strapi is the leading open source headless CMS it's 100% Javascript
+            and fully custimizable
+          </p>
+        </div>
+      </div>
     </>
   );
 };
