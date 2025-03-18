6ea54754da9b07592a75a44de3ccd803be0db542
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-18 11:07:57 +0100

    implement Submenu component to display dynamic links based on current pageId; enhance layout for better presentation

--- 13-strapi-submenus/starter/src/components/Submenu.jsx
+++ 13-strapi-submenus/starter/src/components/Submenu.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import sublinks from "../data";
+import { useGlobalContext } from "../context/GlobalContext";
 
 const Submenu = () => {
+  const { pageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+  console.log(currentPage);
+
   return (
     <>
-      <h2>Submenu</h2>
+      <div className="submenu">
+        <h5>{currentPage?.page}</h5>
+        <div
+          className="submenu-links"
+          style={{
+            gridTemplateColumns:
+              currentPage?.links.length > 3 ? "1fr 1fr" : "1fr",
+          }}
+        >
+          {currentPage?.links?.map((link) => {
+            const { id, url, label, icon } = link;
+
+            return (
+              <a href={url} key={id}>
+                {icon} {label}
+              </a>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
