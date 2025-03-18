66b08ff0dc9eed551985b97ae5ef977a61c22239
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-18 09:28:43 +0100

    add global context for sidebar management and update app structure

--- 13-strapi-submenus/starter/src/App.jsx
+++ 13-strapi-submenus/starter/src/App.jsx
@@ -1,4 +1,15 @@
+import React from "react";
+import { useGlobalContext } from "./context/GlobalContext";
+
 const App = () => {
-  return <h2>Strapi Starter</h2>;
+  const { isSidebarOpen, openSidebar } = useGlobalContext();
+  console.log(isSidebarOpen);
+
+  return (
+    <div>
+      <h2>Strapi Starter</h2>
+    </div>
+  );
 };
+
 export default App;
