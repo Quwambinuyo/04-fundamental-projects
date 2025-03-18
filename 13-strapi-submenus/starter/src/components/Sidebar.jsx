cb7143b6b858f8bbb854c2b05569934843e8610e
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-18 11:26:15 +0100

    refactor Sidebar component for improved structure and styling; enhance CSS for better sidebar visibility and transitions

--- 13-strapi-submenus/starter/src/components/Sidebar.jsx
+++ 13-strapi-submenus/starter/src/components/Sidebar.jsx
@@ -4,41 +4,36 @@ import sublinks from "../data";
 import { useGlobalContext } from "../context/GlobalContext";
 
 const Sidebar = () => {
-  const { closeSidebar, isSidebarOpen } = useGlobalContext();
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
   return (
-    <>
-      <aside className="sidebar">
-        <div className="sidebar-container">
-          <button className="close-btn">
-            <FaTimes />
-          </button>
-
-          <div className="sidebar-links">
-            {sublinks.map((item) => {
-              const { links, page, pageId } = item;
-
-              return (
-                <article key={pageId}>
-                  <h4>{page}</h4>
-                  <div className="sidebar-sublinks">
-                    {links.map((link) => {
-                      const { url, icon, label, id } = link;
-                      return (
-                        <a key={id} href={url}>
-                          {icon}
-                          {label}
-                        </a>
-                      );
-                    })}
-                  </div>
-                </article>
-              );
-            })}
-          </div>
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
+      <div className="sidebar-container">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item) => {
+            const { links, page, pageId } = item;
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link) => {
+                    const { url, icon, label, id } = link;
+                    return (
+                      <a key={id} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
         </div>
-      </aside>
-    </>
+      </div>
+    </aside>
   );
 };
-
 export default Sidebar;
